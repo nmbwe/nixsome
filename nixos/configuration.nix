e09924eed02c12792af62df1ed46383467b8c60d
joaoleal <jgleal@protonmail.com>
2024-03-15 21:25:20 -0300

    Some cleaning and changes
    Signed-off-by: joaoleal <jgleal@protonmail.com>

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -3,14 +3,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-    
-      
       ./hardware-configuration.nix
-      
-      
-
-	#Developer environment settings and programs
-	#./devenv.nix    
   ];
 ##Read it
 networking.hostName = "TheChosenOne";
@@ -23,10 +16,6 @@ boot.loader.efi.canTouchEfiVariables = true;
 # enables flakes
 nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-nixpkgs.config.permittedInsecurePackages = [
-                "openssl-1.1.1w"
-              ];
-
 nixpkgs.config.allowUnfree = true; 
 nixpkgs.config.unstable = {
     # The URL for the unstable channel
@@ -34,19 +23,6 @@ nixpkgs.config.unstable = {
     # Use the latest revision of the channel
     channel = "nixos-unstable";
   };
-
-# Basic Network config for this shit work.
-# it doesnt work for me without it.
-
-networking.useDHCP = lib.mkForce  true;
-networking.networkmanager= lib.mkForce {
-	enable = true; 
-	insertNameservers = [
-	"9.9.9.9" "149.112.112.112"
-	"1.1.1.1" "1.0.0.1"
-	"8.8.8.8" "8.8.4.4" 
-	];
-};
 #Time zone.
 time.timeZone = "America/Sao_Paulo";
 
@@ -57,8 +33,7 @@ services.xserver.enable = true;
 services.xserver.displayManager.lightdm.enable = true;
 services.xserver.desktopManager.cinnamon.enable = true;
 
-
-
+#TODO -  add pt-br keyboard and toogle key
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
   # console = {
@@ -67,29 +42,22 @@ services.xserver.desktopManager.cinnamon.enable = true;
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
-# Uncomment to Use X11
-# services.xserver.enable = true;
-# Configure keymap in X11
-# services.xserver.xkb.layout = "us";
-# services.xserver.xkb.options = "eurosign:e,caps:escape";
-
 # Enable sound.
 sound.enable = true;
 hardware.pulseaudio.enable = true;
 
 #Ensure suport for mousepad
 services.xserver.libinput.enable = true;
+
+#Fiat Piak 
 services.flatpak.enable = true;
-  
+ 
+
+#Who, im 2024 let someone else use its computer ? BEEING AN NIXOS ?????????
 users.users.joaoleal = {
 	isNormalUser = true;
 	extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-	     packages =   [
-	     	pkgs.firefox
-     		];
 };
-
-programs.hyprland.enable = true;
 
 programs.direnv = {
     enable = true;
@@ -106,66 +74,25 @@ programs.direnv = {
 environment.systemPackages = with pkgs;
 [
 	kitty
-  vim
 	nano
 	wget
 	git
-  jq
-  signal-desktop
+    signal-desktop
 	alacritty
 	zsh
 	home-manager
 	tmux
-	lunarvim
-  vscode
-  docker
-  (python3.withPackages(python-pkgs: [
+    vscode
+    docker
+    (python3.withPackages(python-pkgs: [
       python-pkgs.ecdsa
     ]))
-	nodejs_21
-	rustup
-	helix
-  bitcoin
     discord
-  shutter
-  jdk20
-  libgcc
-  gcc
-  wineWowPackages.waylandFull
-  sublime4
-  google-chrome
-  brave
-  ##i, joão leal, am not using this... it work related, i dont know anything about it
-  mullvad-vpn
-  mullvad
-  godot3
-  openssl
-  openssl.dev  
-  pkg-config
-  libiconv
-  trunk  
-  gnupg
-  pinentry
-  firecracker
-  firectl  
-  zola
-  
-  #Language Servers
-  
-  ##HTMl,CSS,JS
-  vscode-langservers-extracted
-  ##Lua
-  lua-language-server
-  ##MD
-  marksman
-  ##Rust
-  rust-analyzer
-  ##C/C++
-  llvmPackages_17.clang-unwrapped
-  ##Nix
-  nil
-  #BASH
-  nodePackages_latest.bash-language-server
+    brave
+    ##i, joão leal, am not using this... it work related, i dont know anything about it
+    mullvad-vpn
+    mullvad
+    gnupg
 ];
   
   users.defaultUserShell = pkgs.zsh;  	
