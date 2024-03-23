e8b0347bfa73bd83af3904026c134843371b6cfb
joaoleal <jgleal@protonmail.com>
2024-03-23 13:40:05 -0300

    standard flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,21 @@
-## Flakes
-
 {
   description = "My NixOS configuration";
-    inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-      polymc.url = "github:PolyMC/PolyMC";
+  
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.TheChosenOne = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./nixos/mod/de.nix
+        ./nixos/mod/system.nix
+        ./nixos/mod/packages.nix
+        ./nixos/mod/bitcoin.nix
+      ];
     };
-    nixpkgs.overlays = [ inputs.polymc.overlay ];
-    outputs = { self, nixpkgs }: { 
-      nixosConfigurations.TheChosenOne = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./nixos/mod/de.nix
-          ./nixos/mod/system.nix
-          ./nixos/mod/packages.nix
-          ./nixos/mod/bitcoin.nix
-          ];
-      };
-      environment.systemPackages = with pkgs; [ polymc ];
   };
 }
