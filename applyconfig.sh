ce3f1d1a4360dd765d1b89f362aa24a33736cf30
jaoleal <jgleal@protonmail.com>
2024-05-05 11:00:19 -0300

    Applychanges better including mome hamager
    Signed-off-by: jaoleal <jgleal@protonmail.com>

--- applyconfig.sh
+++ applyconfig.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 ## This is a temporary way to just move the configuration to /etc/nixos/
-## and apply them.
-
-## Requires sudo permition
-
-echo "Updating NixOS Configuration with the actual flake.nix"
-
-## Copy the system aproprieate hardware-config.nix to this repo to copy back later
-
+## and apply them .
+echo "Updating NixOS and Home Manager Configuration with the actual flake.nix"
+sleep 1 && clear
+## Copy the system apropriate hardware-config.nix to this repo to copy back later
+## Just to not break anything, NixOs has a generated file to handle "Hardware shit"
 cp /etc/nixos/hardware-configuration.nix ./nixos/
+if [ -d "$HOME/.config" ]; then
+    cp -r ./home-manager/ "$HOME/.config" 
+    cp -r ./* /etc/nixos/
+    nixos-rebuild --use-remote-sudo --verbose 0  switch  > /dev/null
 
-## copy the whole directorie so i dont need to handle with paths now.
-cp -r ./* /etc/nixos/
-nixos-rebuild switch
+    clear
+    echo "Done my bro"
+else
+    echo "dot config just does not exists pal, cannot copy Home-manager config"
+fi
