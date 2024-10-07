55f4a850dc54a5098860e98cb98c0dd9cb4fc3ad
= <=>
2024-10-07 11:21:46 -0300

    Updating nix files to evaluate a full nixos system

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-  };
+    };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }:
+  outputs = { self, nixpkgs, ... }:
     let 
       system = "x86_64-linux";
       pkgs = import nixpkgs {system = system; };
@@ -12,30 +11,13 @@
     in
     {
     nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
+      mainConfig = nixpkgs.lib.nixosSystem {
         system = system;
-
-        modules = [
-          nixos-wsl.nixosModules.default
-          {
-            
-            system.stateVersion = "24.05";
-          }
-          common.nixosConfig 
+	modules = [
           common
         ];
       };
-    };
-      packages = {
-        "${system}" = pkgs.buildEnv {
-          name = "my-env";
-          paths = common.systemPackages;
-        };
-      };
-
-      # Default package for `nix run` or `nix develop`.
-      defaultPackage.${system} = self.packages.${system};
-  };
-
+    };        
   description = "Remember the DISSSSS ?";
+	};
 }
--- main.nix
+++ main.nix
@@ -1,6 +1,7 @@
 { lib, pkgs, ... }:
 {
-  systemPackages = with pkgs; [
+	imports = [ ./hardware-configuration.nix];  
+environment.systemPackages = with pkgs; [
     wget
     vim
     just
@@ -23,21 +24,58 @@
     };
   };
 
-  nixosConfig = lib.mkIf (pkgs.stdenv.isLinux && pkgs.stdenv.isNixos) {
-    wsl = {
-      enable = true;
-      defaultUser = "JoaoLeal";
-    };
-    networking.hostName = "nixos";
-    time.timeZone = "UTC";
-    users.users.JoaoLeal = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
-    };
-    services.udev.packages = [ pkgs.yubikey-personalization ];
-    services.pcscd.enable = true;
-    hardware.gpgSmartcards.enable = true;
+  users.users.jaoleal = {
+    isNormalUser = true;
+    description = "Joao Leal";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
   };
+  services.xserver = { 
+	enable = true;
+	desktopManager.gnome.enable = true;
+	displayManager.gdm.enable = true;
+};
+  nixpkgs.config.allowUnfree = true;
+  networking.hostName = "nixos";
+  services.udev.packages = [ pkgs.yubikey-personalization ];
+  services.pcscd.enable = true;
+  hardware.gpgSmartcards.enable = true;
+  networking.networkmanager.enable = true;
+  time.timeZone = "America/Sao_Paulo";
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    };
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+	boot= {
+	  initrd.luks.devices."luks-8970f121-a76c-44b7-8be8-9c5ad0d4a229".device = "/dev/disk/by-uuid/8970f121-a76c-44b7-8be8-9c5ad0d4a229";
+loader = {
+		systemd-boot.enable = true;
+		efi.canTouchEfiVariables = true;
+	};};
+	  system.stateVersion = "24.05"; # Did you read the comment?
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
+
